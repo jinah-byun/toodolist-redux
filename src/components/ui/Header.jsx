755a3d50a85a6d2a0cda3de7538d40b073a5ac06
Jina <118155022+jinah-byun@users.noreply.github.com>
2023-05-10 21:06:34 +0900

    Update Header.jsx

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import styled from "styled-components";
+import "./곰돌이 푸.png";
 
 const Header = () => {
   return (
-    <StContainer>
-      <div>My Todo List</div>
-      <div>React</div>
-    </StContainer>
+    <div>
+      <StContainer>
+        <div>My Todo List</div>
+        <div>React</div>
+      </StContainer>
+      <div>
+        <img
+          src={require("./곰돌이 푸.png")}
+          width="1200"
+          height="300"
+          alt="testA"
+        />
+      </div>
+    </div>
   );
 };
 export default Header;
 
 const StContainer = styled.div`
   border: 1px solid #ddd;
+  border-radius: 10px 10px 0 0;
   height: 60px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 20px;
-  margin-bottom: 24px;
+  background-color: teal;
+  font-size: large;
+  font-weight: 20px;
+  color: white;
 `;
